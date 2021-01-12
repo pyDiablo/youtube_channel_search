27320fae6e5f138dd7b8179941ba9cc578bc767f
krazykirby99999 <krazykirby99999@gmail.com>
2021-01-12 11:01:32 -0600

    Formatting

--- youtube_search.py
+++ youtube_search.py
@@ -17,35 +17,41 @@ from googleapiclient.discovery import build
 import os
 import time
 
+
 def connect_yt_api():
     api_key = os.environ['YT_API_KEY']
-    
+
     youtube = build('youtube', 'v3', developerKey=api_key)
+
 
 def search_yt_channel(yt_api_client):
     # Search a YouTube channel for videos with {search terms here} in title
     youtube = yt_api_client
     request = youtube.search().list(
-        part='snippet',  # string, The *part* parameter specifies a comma-separated list of one or more search resource properties that the API response will include. Set the parameter value to snippet. (required)
+        # string, The *part* parameter specifies a comma-separated list of one or more search resource properties that the API response will include. Set the parameter value to snippet. (required)
+        part='snippet',
         channelId='CHANNEL_ID_HERE',  # string, channel's id you want to search
         maxResults=25,  # integer, No. of results you want to be displayed
-        order='viewCount',  # Allowed values: searchSortUnspecified, date, rating, viewCount, relevance, title, videoCount
+        # Allowed values: searchSortUnspecified, date, rating, viewCount, relevance, title, videoCount
+        order='viewCount',
         q='SEARCH_TERMS_HERE',  # string, Textual search terms to match
-        )
+    )
     response = request.execute()
     return response
+
 
 def extract_yt_video_id_from_data(yt_api_response):
     response = yt_api_response
     video_data = response['items']  # Extratcing video info from json data
     video_ids = []  # List of video ids
     # count = 0  count of urls currently downloading
- 
+
     # Extracting video ids from json data
     for i in range(1, len(video_data)):
         video_ids.append(video_data[i]['id']['videoId'])
-    
+
     return video_ids
+
 
 def create_yt_links_from_ids(video_ids):
     # Creating YouTube video links and adding them to a list
@@ -53,19 +59,22 @@ def create_yt_links_from_ids(video_ids):
     for id in video_ids:
         url = f'https://youtube.com/watch?v={id}'
         video_urls.append(url)
-    
+
     return video_urls
+
 
 def download_yt_videos_from_list(video_urls):
     # Dowload each video one by one using youtube-dl
     # # for url in video_urls:  Alternate way of doing the following
-    for i, url in enumerate(video_urls, 1):  # enumerate(iterable, start) adds an index 'i' to the iterable (e.g. lists) and returns a tuple
-    # count = count + 1
-    # print(f'Downloading {count} of {len(video_urls)}: {url}...')  Alternate way of doing the above
+    # enumerate(iterable, start) adds an index 'i' to the iterable (e.g. lists) and returns a tuple
+    for i, url in enumerate(video_urls, 1):
+        # count = count + 1
+        # print(f'Downloading {count} of {len(video_urls)}: {url}...')  Alternate way of doing the above
         print(f'Downloading {i} of {len(video_urls)}: {url}...')
         os.system(f'youtube-dl {url}')
         print('Pausing the execution for 5 mins\n')
         time.sleep(300)  # Wait for 5 mins (300 seconds)
+
 
 def main():
     client = connect_yt_api()
@@ -74,3 +83,7 @@ def main():
     urls = create_yt_links_from_ids(ids)
     download_yt_videos_from_list(urls)
     print('Finished!')
+
+
+if __name__ == '__main__':
+    main()
